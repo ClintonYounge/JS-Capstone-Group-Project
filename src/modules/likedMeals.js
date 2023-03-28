07b4ee9bea91a67a8a61a52b42043789e08e636c
ClintonYounge <clintonyounge18@gmail.com>
2023-03-28 18:36:23 -0400

    Update JS Code

--- src/modules/likedMeals.js
+++ src/modules/likedMeals.js
@@ -1,12 +1,19 @@
 const URL_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NSQ2jUNcTp3W8mj5Cd8P/likes';
 const mealContainer = document.querySelector('.meal-cards');
 
+const fetchLikes = async (item) => {
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  const likeCount = document.querySelector(`.likes-${item}`);
+  likeCount.querySelector('span').textContent = `${data.filter((items) => items.item_id === item)[0].likes} likes`;
+};
+
 const likes = () => {
-  mealContainer.addEventListener('click', (e) => {
+  mealContainer.addEventListener('click', async (e) => {
     const clicked = e.target.closest('.like-icon');
     if (!clicked) return;
     const item = clicked.getAttribute('id');
-    fetch(URL_API, {
+    await fetch(URL_API, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
@@ -16,15 +23,7 @@ const likes = () => {
         item_id: item,
       }),
     });
-    const fetchLikes = async () => {
-      const response = await fetch(URL_API);
-      const data = await response.json();
-      const likeCount = document.querySelector(`.likes-${item}`);
-      likeCount.querySelector('span').textContent = `${
-        data.filter((items) => items.item_id === item)[0].likes
-      } likes`;
-    };
-    fetchLikes();
+    await fetchLikes(item);
   });
 };
 
