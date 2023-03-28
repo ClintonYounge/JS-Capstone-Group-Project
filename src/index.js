30e2ccd587ed0c76e3b0fbdf2e78be6183adaab4
ClintonYounge <clintonyounge18@gmail.com>
2023-03-28 13:52:00 -0400

    Fetch and display meals

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import logoIcon from '../assets/logo.png';
+import display from './modules/displayMeals.js';
 
 const logo = document.querySelector('.logo');
 logo.src = logoIcon;
+
+display();
