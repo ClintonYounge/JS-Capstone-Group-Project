7e87caa4c478ef031bab57ce117ce9722db750c1
Clinton Younge <clintonyounge18@gmail.com>
2023-03-29 17:08:29 -0400

    Revert "Add functionality display likes"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import './style.css';
 import logoIcon from '../assets/logo.png';
 import display from './modules/displayMeals.js';
-import { displayLikes, likes } from './modules/likedMeals.js';
 
 const logo = document.querySelector('.logo');
 logo.src = logoIcon;
 
 display();
-likes();
-displayLikes();
